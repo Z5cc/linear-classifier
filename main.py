d003d0ded25697de9a2bb2ea4eb1972db3869f70
david <d.ncklsr@gmail.com>
2023-04-03 12:29:03 +0200

    plotting

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-#Und lernen Sie dann für Data3.csv (-1 / +1 2d-Punkte) einen linearen Klassifikator - (zB mit Pytorch oder Tensorflow).
 import pandas as pd
 import numpy as np
-np.set_printoptions(precision=3, suppress=True)
 import tensorflow as tf
 from tensorflow.keras import layers
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-#load a dataset
-data3_train = pd.read_csv('Data3.csv', sep=';', names=["x", "y", "label"])
-data3_features = data3_train.copy()
-data3_labels = data3_features.pop("label")
-data3_labels = (data3_labels + 1)/2
-data3_features = (data3_features + 2)/10
-data3_features = np.array(data3_features)
+# load training data
+train_data = pd.read_csv('Data3.csv', sep=';', names=["x", "y", "label"])
+train_features = train_data.copy()
+train_labels = train_features.pop("label")
+train_labels = (train_labels + 1)/2
+train_features = (train_features + 2)/10
+train_features = np.array(train_features)
+
+# load testing data
+test_data = pd.read_csv('Data3.csv', sep=';', names=["x", "y", "label"])
+test_features = test_data.copy()
+test_labels = test_features.pop("label")
+test_labels = (test_labels + 1)/2
+test_features = (test_features + 2)/10
+test_features = np.array(test_features)
 
 
-#build a machine learning project
+
+
+# build a machine learning project
 model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(1)
 ])
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
 model.compile(optimizer='adam',
-              loss=loss_fn,
+              loss=tf.keras.losses.MeanSquaredError(),
               metrics=['accuracy'])
 
 
-# #train and evaluate your model
-model.fit(data3_features, data3_labels, epochs=1)
+# train and evaluate your model
+model.fit(train_features, train_labels, epochs=3)
 #
-# model.evaluate(x_test,  y_test, verbose=2)
+model.evaluate(x_test,  y_test, verbose=2)
 #
 # probability_model = tf.keras.Sequential([
 #   model,
@@ -36,3 +46,14 @@ model.fit(data3_features, data3_labels, epochs=1)
 # ])
 #
 # print(probability_model(x_test[:5]))
+
+
+# # plot
+# sns.scatterplot(data=train_data, x='x', y='y', hue='label', style='predicted_label')
+#
+# # TODO: get weights from model
+# boundary_x = np.linspace(np.min(train_data.x), np.max(train_data.x), 100)
+# boundary_y = -(weights[0] * boundary_x + bias) / weights[1]
+# sns.lineplot(x=boundary_x, y=boundary_y)
+#
+# plt.show()
